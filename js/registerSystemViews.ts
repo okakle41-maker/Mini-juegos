97b2f5c3d20b45a348edf326ed74da263369a2cc
okakle41 <okakle41@gmail.com>
2026-08-02 20:06:22 -0300

    Reapply "fix onl menu"
    This reverts commit 5548ea3c4a8edbe3d51bafa5bdeafa53d193c606.

--- js/registerSystemViews.ts
+++ js/registerSystemViews.ts
@@ -122,7 +122,6 @@ export function registerSystemViews(): void {
     num: 'SYS-08',
     description: 'Selección de módulos con soporte multiplayer',
     difficulty: 0,
-    css: 'css/onlineLobbyConfig.css',
     hidden: true,
     logic: async () => {
       const { init, stop } = await import('./views/onlineLobby.logic.js');
